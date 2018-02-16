b59a08f58bb709cf9e423548b60d0e773038a40a
Alexiuce <alexiuce@qq.com>
2018-02-16 17:13:41 +0800

    add Login{Window,View} Controller

---
+++ Folder/SwiftQQ/SwiftQQ/Login/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  SwiftQQ
+//
+//  Created by Alexcai on 2018/2/16.
+//
+
+import Cocoa
+
+class LoginViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ Folder/SwiftQQ/SwiftQQ/Login/LoginWindowController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginWindowController.swift
+//  SwiftQQ
+//
+//  Created by Alexcai on 2018/2/16.
+//
+
+import Cocoa
+
+class LoginWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
