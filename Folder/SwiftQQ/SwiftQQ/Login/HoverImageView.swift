55967a012d132021aaeca610d1db7c82b9bdc626
Alexiuce <alexiuce@qq.com>
2018-02-17 14:06:34 +0800

    add HoverImageView

---
+++ Folder/SwiftQQ/SwiftQQ/Login/HoverImageView.swift
@@ -0,0 +1,48 @@
+//
+//  HoverImageView.swift
+//  SwiftQQ
+//
+//  Created by Alexcai on 2018/2/17.
+//
+
+import Cocoa
+
+class HoverImageView: NSImageView {
+
+    lazy var normalImage: NSImage = {
+        let img = NSImage(named: NSImage.Name(rawValue: "head-normal"))
+        return img!
+    }()
+    
+    lazy var hoverImage : NSImage = {
+        let img = NSImage(named: NSImage.Name(rawValue: "circle-big"))
+        return img!
+    }()
+    lazy var borderLayer: CALayer = {
+        let border = CALayer()
+        border.frame = self.bounds
+        border.contents = self.normalImage
+        return border
+    }()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer?.cornerRadius = bounds.size.width * 0.5
+        layer?.addSublayer(borderLayer)
+        
+        // 1. 添加鼠标监听
+        let trackArea = NSTrackingArea(rect: bounds, options: [.activeAlways,.mouseEnteredAndExited], owner: self, userInfo: nil)
+        addTrackingArea(trackArea)
+    }
+    
+    override func mouseEntered(with event: NSEvent) {
+        print("mouse enter")
+        borderLayer.contents = hoverImage
+        
+    }
+    
+    override func mouseExited(with event: NSEvent) {
+        print("mouse exit")
+        borderLayer.contents = normalImage
+    }
+}
