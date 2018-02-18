e980abe5312f327dcc434ddf601d0b9554148071
Alexiuce <alexiuce@qq.com>
2018-02-18 15:34:32 +0800

    add titlebar update

--- Folder/SwiftQQ/SwiftQQ/Home/HomeWindowController.swift
+++ Folder/SwiftQQ/SwiftQQ/Home/HomeWindowController.swift
@@ -6,31 +6,36 @@
 //
 
 import Cocoa
+import SnapKit
 
 class HomeWindowController: NSWindowController {
-
-    lazy var themeView: NSView = {
-        let view = self.window?.standardWindowButton(NSWindow.ButtonType.closeButton)?.superview
-        return view!
-    }()
 
     override func windowDidLoad() {
         super.windowDidLoad()
         window?.isMovableByWindowBackground = true
       
-       
+        updateWindowTitleBar()
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     }
     
     fileprivate func updateWindowTitleBar(){
-       
+        
+        let themeView = window?.contentView?.superview
+        let titleView = themeView?.subviews.last
+        titleView?.autoresizesSubviews = true
+        titleView!.snp.makeConstraints { (make) in
+            make.top.equalTo(23)
+            make.left.equalTo(10)
+            make.width.equalTo(60)
+            make.height.equalTo(22)
+        }
         
     }
 }
 
 extension HomeWindowController: NSWindowDelegate{
     func windowDidResize(_ notification: Notification) {
-       
+       updateWindowTitleBar()
     }
     
    
