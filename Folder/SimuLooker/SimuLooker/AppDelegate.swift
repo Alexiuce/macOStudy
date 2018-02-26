6f88eedf2c16e1f7fabad29d1668a3a7d286477a
Alexiuce <alexiuce@qq.com>
2018-02-26 10:05:00 +0800

    add simulooker project

---
+++ Folder/SimuLooker/SimuLooker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SimuLooker
+//
+//  Created by caijinzhu on 2018/2/26.
+//  Copyright © 2018年 alexiuce.github.io. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
