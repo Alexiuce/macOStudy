112242611e8091c6e7ff17fadf25ff388d90b4a8
Alexiuce <alexiuce@qq.com>
2018-02-26 11:40:43 +0800

    add ActionTrampoline

--- Folder/SimuLooker/SimuLooker/AppDelegate.swift
+++ Folder/SimuLooker/SimuLooker/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        let button = NSButton()
+        button.setAction(action: {print("action from \($0)")})
+        button.sendAction(button.action, to: button.target)
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
---
+++ Folder/SimuLooker/SimuLooker/ToolKit/ActionTrampline.swift
@@ -0,0 +1,36 @@
+//
+//  ActionTrampline.swift
+//  SimuLooker
+//
+//  Created by caijinzhu on 2018/2/26.
+//  Copyright © 2018年 alexiuce.github.io. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+
+
+let NSControlActionFunctionAssociatedObjectKey = UnsafeMutablePointer<Int8>.allocate(capacity: 1)
+
+
+
+class ActiontTrampoline: NSObject {
+    var action: (NSControl) -> Void
+    init(action: @escaping(NSControl) -> Void) {
+        self.action = action
+    }
+    @objc(action:)
+    func action(sender: NSControl){
+        action(sender)
+    }
+}
+
+extension NSControl{
+    @nonobjc func setAction(action: @escaping (NSControl)->Void){
+        let trampoline = ActiontTrampoline(action: action)
+        target = trampoline
+        self.action = #selector(ActiontTrampoline.action(sender:))
+        objc_setAssociatedObject(self, NSControlActionFunctionAssociatedObjectKey, trampoline, .OBJC_ASSOCIATION_RETAIN)
+    }
+}
