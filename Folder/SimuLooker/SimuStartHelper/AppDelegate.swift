12126457aa62b067b2e8ce71eaaabd4f6782c5ce
Alexiuce <alexiuce@qq.com>
2018-09-04 11:20:24 +0800

    helpAppDelegate update

--- Folder/SimuLooker/SimuStartHelper/AppDelegate.swift
+++ Folder/SimuLooker/SimuStartHelper/AppDelegate.swift
@@ -15,7 +15,32 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        var mainIsRunning = false
+        let mainAppId = "alexiuce.SimuLooker"
+        NSWorkspace.shared.runningApplications.forEach {
+            mainIsRunning = $0.bundleIdentifier == mainAppId
+        }
+        if mainIsRunning {  // 如果主应用已经启动,则退出helper
+            NSApp.terminate(nil)
+        }else{ // 如果主应用未启动,则启动主应用
+    
+            let path = Bundle.main.bundlePath
+            
+            let appPath = path.replacingOccurrences(of: "/Library/LoginItems/SimuStartHelper.app", with:"/MacOS/SimuLooker")
+            
+          
+            
+            if !FileManager.default.fileExists(atPath: appPath) {
+                return;
+            }
+           let runningArray = NSRunningApplication.runningApplications(withBundleIdentifier: mainAppId)
+            if (runningArray.count > 0) {
+                return;
+            }
+            NSWorkspace.shared.launchApplication(appPath)
+            NSApp.terminate(nil)
+        }
+        
        
     }
 
